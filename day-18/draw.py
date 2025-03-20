9fd264e0991b8f1922b7f2fea74a461c7b15f298
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-03-20 12:04:33 -0300

    dia 18 1

---
+++ day-18/draw.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+tartaruga = Turtle()
+
+tartaruga.color("red")
+tartaruga.shape("circle")
+
+tartaruga.forward(100)
+tartaruga.right(45)
+tartaruga.forward(100)
+
+
+
+
+ecra = Screen()
+ecra.exitonclick()
+ 
