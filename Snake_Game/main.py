84d94cf55ef1f430a73dc7c5f00df71ad0527751
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-03-25 18:18:10 -0300

    refactor Snake Game: update snake body shape and implement movement logic

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -1,4 +1,4 @@
-from turtle import Screen, Turtle,colormode
+from turtle import Screen, Turtle, colormode
 import random
 
 tela = Screen()
@@ -6,35 +6,28 @@ tela.title("Jogo da Cobra")
 tela.bgcolor("black")
 tela.setup(width=1200, height=900)
 
-
-
 def corpo_cobra():
     cobra = []
     for i in range(3):
         cobra.append(Turtle())
-        cobra[i].shape("turtle")
+        cobra[i].shape("square")
         cobra[i].color("white")
         cobra[i].penup()
         cobra[i].goto(-20 * i, 0)
     return cobra
 
+cobra = corpo_cobra()
 
+def mover_cobra():
+    for i in range(len(cobra) - 1, 0, -1):
+        new_x = cobra[i - 1].xcor()
+        new_y = cobra[i - 1].ycor()
+        cobra[i].goto(new_x, new_y)
+    cobra[0].forward(20)
+    tela.update()
+    tela.ontimer(mover_cobra, 100)
 
+mover_cobra()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tela.listen()
 tela.exitonclick()
