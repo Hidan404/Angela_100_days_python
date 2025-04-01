af96c0201dd7cecbbaeff7237aa1e592819ad14b
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-03-31 22:45:30 -0300

    add Player class: implement player movement and initialize player position in main.py

--- turtle-crossing-start/main.py
+++ turtle-crossing-start/main.py
@@ -8,7 +8,14 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+jogador = Player()
+screen.listen()
+screen.onkey(jogador.cima(), "Up")
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    
     screen.update()
+     
--- turtle-crossing-start/player.py
+++ turtle-crossing-start/player.py
@@ -1,7 +1,24 @@
+from turtle import Turtle , Screen
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        
+        
+        
+
+    def cima(self):
+        #new_y = self.ycor() + MOVE_DISTANCE
+        #self.goto(self.xcor(), new_y)
+        self.forward(MOVE_DISTANCE)
+
+
