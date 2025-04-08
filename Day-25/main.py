286787c4591d67b86808b27d834e6f219b88924e
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-04-07 23:07:59 -0300

    update weather data processing: enhance CSV reading with UTF-8 encoding, add data summary outputs, and handle temperature values

--- Day-25/main.py
+++ Day-25/main.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
 
-dados = pd.read_csv("Day-25/weather_data.csv", sep=";")
-print(dados)
+dados = pd.read_csv("Day-25/weather_data.csv", sep=",", encoding="utf-8")
+print(dados.info())
+print(dados.head())
+print(dados.describe())
+print(dados.columns)
+
+for d in dados["temp"]:
+    temperatura = []
+    if isinstance(d, (int, float)):
+        temperatura.append(d)
+    else:
+        temperatura.append(0)
+    print(temperatura)
