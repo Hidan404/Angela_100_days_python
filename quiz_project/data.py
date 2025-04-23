3fe1a4f5e8630b5518e72deecf1e1f41814e3210
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-04-23 10:28:39 -0300

    update data

--- quiz_project/data.py
+++ quiz_project/data.py
@@ -1,17 +1,9 @@
 import requests
 
 
-URL = "https://opentdb.com/api.php?amount=50&category=15&difficulty=medium&type=boolean"
 
 
-response = requests.get(URL)
-data = response.json()  
 
-
-if "results" in data:
-    question_data = data["results"]
-else:
-    question_data = []  
 
 
 
