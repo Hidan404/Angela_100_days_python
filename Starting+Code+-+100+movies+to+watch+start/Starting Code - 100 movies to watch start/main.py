253fdbddc13fb0dc811f0adacc705407d6bb7c6a
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-05-08 22:36:57 -0300

    Add movie titles to filmes.json and update main.py to save titles to the file

--- Starting+Code+-+100+movies+to+watch+start/Starting Code - 100 movies to watch start/main.py
+++ Starting+Code+-+100+movies+to+watch+start/Starting Code - 100 movies to watch start/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
@@ -10,8 +11,10 @@ resposta = requests.get(url=URL).text
 soup = BeautifulSoup(resposta, "html.parser")
 
 titulos = [t.get_text() for t in soup.find_all(name="h3",class_="title")]
-for t in reversed(titulos):
-    print(t)
+
+with open("Starting+Code+-+100+movies+to+watch+start/filmes.json", "a") as f:
+    for t in reversed(titulos):
+        f.write(json.dumps(t, ensure_ascii=False, indent=4) + "\n")
 
 
 
