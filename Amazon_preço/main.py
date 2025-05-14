efb61a9ded401eb17d6b7ef9c79de765fbac6b88
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-05-13 22:20:34 -0300

    Refactor price scraping logic and update email notification settings; remove unused Spotify API class

--- Amazon_preço/main.py
+++ Amazon_preço/main.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import datetime as dt
+import os
 
 
-url = "https://appbrewery.github.io/instant_pot/"
+url = "https://www.amazon.com.br/Mouse-Nanoreceptor-Inclusa-Logitech-Mouses/dp/B00UC1R80C/ref=pd_bxgy_thbs_d_sccl_2/137-0836641-4032029"
 resposta = requests.get(url= url).text
 
 soup = BeautifulSoup(resposta, "html.parser")
@@ -19,15 +21,33 @@ for preco in precos_div:
         precos.append(valor)
     except AttributeError:
         continue  # Se não encontrar uma parte, ignora esse bloco
+print(precos)
 
-preco_selecionado = float(precos[0].replace("..","."))
+precos_convertidos = []
+for preco in precos:
+    preco_float = float(preco.replace(".", "").replace(",", "."))
+    precos_convertidos.append(preco_float)
+    print(preco_float)
+
+
+preco_selecionado = 0
+for preco in precos:
+    if preco == 69.9:
+        preco_selecionado = preco
+        print(preco_selecionado)
+
+print(preco_selecionado)
 
 if preco_selecionado < 100:
-    meu_email = "seu_email@gmail.com"
-    senha = "sua_senha"
+    meu_email = "ronaldkurouzo@gmail.com"
+    senha = os.getenv("SENHA_EMAIL")
 
     with smtplib.SMTP("smtp.gmail.com", 587) as conexao:
         conexao.starttls()
-        conexao.login(user=meu_email, password=senha)
+        conexao.login(user=meu_email, password=senha,)
         mensagem = f"Subject:Alerta de Preço\n\nO preço caiu para {preco_selecionado}!"
-        conexao.sendmail(from_addr=meu_email, to_addrs="destinatario_email@gmail.com", msg=mensagem)
+        conexao.sendmail(from_addr=meu_email, to_addrs="ronald.dev404@gmail.com", msg=mensagem)
+
+
+
+        
--- Maquina_tempo_Musica/api_spotify.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class DadosApi():
-    def __init__(self):
-        self.CLIENTE_ID = os.getenv("MTM_ID")
-        self.CLIENTE_SECRET = os.getenv("MTM_SECRET")
-        self.url = 'http://127.0.0.1:8888/callback'
-        self.ESCOPO = "user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-private user-read-email streaming"
