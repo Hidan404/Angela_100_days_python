c173187cd4ddc255b3fdc712f3e30066adcf1ede
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-05-14 13:26:28 -0300

    Refactor price retrieval logic in Amazon scraping; remove unnecessary wait condition

---
+++ Selenium/hashtag.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import time
+
+navegador = webdriver.Chrome()
+navegador.get("https://www.instagram.com/")
+
+
+
+time.sleep(5)
--- Selenium/main.py
+++ Selenium/main.py
@@ -10,9 +10,9 @@ opcoes_chrome.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=opcoes_chrome)
 driver.get("https://www.amazon.com.br/animal-social-Elliot-Aronson/dp/8576576007")
 
-preco_real = WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.CLASS_NAME, 'a-price-whole'))
-)
-preco_inteiro =preco_real.text
+preco_real = driver.find_element(By.CSS_SELECTOR,"span.a-price.aok-align-center span.a-offscreen")
+# Espera até que o elemento esteja presente
+print(preco_real.text)
 
-print(preco_inteiro)
+
+print(preco_real.text)
