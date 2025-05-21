ece36eb6f711ba069a830a4196df276cf6c2c044
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-05-20 23:20:57 -0300

    decorators 2

--- Flask/Project_higher_lower_game/main.py
+++ Flask/Project_higher_lower_game/main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/username/<nome>/1")
 def cumprimento(nome):
-    return f"<h1>Olá, {nome}!</h1>"
+    return f"<h1 style='color: blue;'>Olá, {nome}!</h1>"\
+           f"<h2 style='color: green;'>Bem-vindo ao nosso site!</h2>"\
+           f"<p style='color: purple;'>Estamos felizes em tê-lo aqui.</p>"\
+           f"<p style='color: orange;'>Divirta-se navegando!</p>"\
+           f"<img src='https://media.giphy.com/media/3o7aD2sa1v4x5q0g8I/giphy.gif' alt='giphy' width='300' height='200'></img>"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Flask/teste_flask.py
+++ Flask/teste_flask.py
@@ -3,8 +3,34 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+def negrito(func):
+    def wrapper(*args, **kwargs):
+        response = func(*args, **kwargs)
+        if isinstance(response, str):
+            return f"<strong>{response}</strong>"
+        return response
+    return wrapper
+
+def sublinhado(funcao):
+    def wrapper(*args, **kwargs):
+        resposta = funcao(*args, **kwargs)
+        if isinstance(resposta, str):
+            return f"<u>{resposta}</u>"
+        return resposta
+    return wrapper
+
+def emfase(funcao):
+    def wrapper(*args, **kwargs):
+        resposta = funcao(*args, **kwargs)
+        if isinstance(resposta, str):
+            return f"<em>{resposta}</em>"
+        return resposta
+    return wrapper
 
 @app.route("/")
+@negrito
+@sublinhado
+@emfase
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
