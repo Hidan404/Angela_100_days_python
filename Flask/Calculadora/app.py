2049ae7ee691b76b8e19eb5584ff9162769df796
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-05-22 14:01:09 -0300

    Implement calculator functionality with addition, subtraction, multiplication, and division; add HTML template for user input and results display.

--- Flask/Calculadora/app.py
+++ Flask/Calculadora/app.py
@@ -1,18 +1,30 @@
-# TODO: Create the logging_decorator() function 👇
-def logging_decorator(funcao):
-    def wrapper(*args):
-        args_str = ",".join(str(arg) for arg in args)
-        print(f"You called a_function({args_str})")
-        resultado = funcao(*args)
-        print(f"It returned: {resultado}")
-        return resultado
-    return wrapper
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 
-# TODO: Use the decorator 👇
+@app.route('/', methods=['GET', 'POST'])
+def calculadora():
+    if request.method == "POST":
+        numero1 = float(request.form["numero1"])
+        numero2 = float(request.form["numero2"])
+        operacao = request.form["operacao"]
 
-@logging_decorator
-def a_function(*args):
-    return sum(args)
-    
-a_function(1,2,3)
+        if operacao == "soma":
+            resultado = numero1 + numero2
+        elif operacao == "subtracao":
+            resultado = numero1 - numero2
+        elif operacao == "multiplicacao":
+            resultado = numero1 * numero2
+        elif operacao == "divisao":
+            if numero2 != 0:
+                resultado = numero1 / numero2
+            else:
+                resultado = "Erro: Divisão por zero"
+        else:
+            resultado = "Operação inválida"
+        return render_template("index.html", resultado=resultado)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
